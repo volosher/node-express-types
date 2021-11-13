383f1891d16b6e321ad7109fb02f007d63822e84
Volodymyr Sheremeta <41182502+volosher@users.noreply.github.com>
2021-11-13 19:35:45 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from 'cors';
 import { connectToDatabase } from "./services/database.service"
 import { favoritesRouter } from "./routes/favorites.router"
 
 
 dotenv.config();
 
+
 const PORT = process.env.PORT || '8080';
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors());
 
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "ejs");
